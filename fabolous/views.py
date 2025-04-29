160efe16a1b728ba66a6c5a45cc3ffce89230564
Sumaya <sumayayasin25@gmail.com>
2025-04-29 14:17:22 +0100

    commit updated version 1

--- fabolous/views.py
+++ fabolous/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse('<h1>Welcome to Home Page</h1>')
+
 
 # Create your views here.
+def home(request):
+    return render(request, 'fabolous/SplashScreen.html')
